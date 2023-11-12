4932e5c284d2d2f69a0d892dbccc310c64ca1ca8
Srtpatil <Samarthpatil15@gmail.com>
2023-11-12 21:07:17 +0530

    updated linked in link

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,6 @@
 
 export const EMAIL = "samarthpatil15@gmail.com";
 export const GITHUB_LINK = "https://github.com/Srtpatil";
-export const LINKEDIN_LINK = "https://www.linkedin.com/in/samarth-patil-6a7877159/";
+export const LINKEDIN_LINK = "https://www.linkedin.com/in/samarth-p-6a7877159";
 export const TWITTER_LINK = "https://twitter.com/myth1cal_blad3";
 export const LEETCODE_LINK = "https://leetcode.com/user7622T/";
