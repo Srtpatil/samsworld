3848aed9b65a42c0f200c5651e44ceb753a4408c
Srtpatil <Samarthpatil15@gmail.com>
2024-06-14 22:55:27 +0530

    Eye animating cat

--- src/components/Cat/Cat.jsx
+++ src/components/Cat/Cat.jsx
@@ -3,41 +3,43 @@ import "./Cat.css"
 import { ReactComponent as CatSVG } from "../../assets/cat.svg"
 
 function Cat() {
+    const [coords, setCoords] = useState({ x: 0, y: 0 });
+    useEffect(() => {
+        const handleWindowMouseMove = e => {
+            const leftEye = document.querySelector('#Left-eye-pupil');
+            const rightEye = document.querySelector('#Right-eye-pupil');
+            const windowHeight = window.innerHeight;
+            const windowWidth = window.innerWidth;
 
-    // useEffect(() => {
-    //     const handleWindowMouseMove = e => {
-    //         const leftEye = document.querySelector('#Left-eye-pupil');
-    //         const rightEye = document.querySelector('#Right-eye-pupil');
-    //         const x = e.offsetX;
-    //         const y = e.offsetY;
-    //         const windowHeight = window.innerHeight;
-    //         const windowWidth = window.innerWidth;
-    //         const max = 3;
+            const max = 3.5;
+            const x = e.clientX
+            const y = e.clientY
+            setCoords({
+                x,
+                y
+            });
+            // Eye movement
+            [leftEye, rightEye].forEach(eye => {
+                if (!eye) {
+                    return
+                }
+                eye.style.transform = `translate(
+                    ${max / (windowWidth / x)}%,
+                    ${max / (windowHeight / y)}%
+                )`;
+            });
 
-    //         // Eye movement
-    //         // [leftEye, rightEye].forEach(eye => {
-    //         //     if (!eye) {
-    //         //         return
-    //         //     }
-    //         //     eye.style.transform = `translate(
-    //         // 	${max / (windowWidth / x)}%,
-    //         // 	${max / (windowHeight / y)}%
-    //         // )`;
-    //         // });
-    //         // setCoords({
-    //         //     x,
-    //         //     y
-    //         // });
-    //     };
-    //     window.addEventListener('mousemove', handleWindowMouseMove);
+        };
 
-    //     return () => {
-    //         window.removeEventListener(
-    //             'mousemove',
-    //             handleWindowMouseMove,
-    //         );
-    //     };
-    // }, []);
+        window.addEventListener('mousemove', handleWindowMouseMove);
+
+        return () => {
+            window.removeEventListener(
+                'mousemove',
+                handleWindowMouseMove,
+            );
+        };
+    }, []);
     return (
         <div className='cat-container'>
             <CatSVG />
